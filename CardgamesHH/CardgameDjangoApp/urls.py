c9c1a993be85bd27c8b42a4e418a74fe6ada4e88
Laith Bohsali <lbohsali@icloud.com>
2022-10-01 13:10:46 -0500

    urls, views

--- CardgamesHH/CardgameDjangoApp/urls.py
+++ CardgamesHH/CardgameDjangoApp/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("blackjack", views.blackjack, name="blackjack"),
+    path("texasholdem", views.texasholdem, name="texasholdem"),
+    path("war", views.war, name="war"),
 ]
