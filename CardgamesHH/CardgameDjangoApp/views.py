9571a41c6c2d6a23c08b2d0ea91e1e05e958803f
Chandler Ngo <chandlerngo00@gmail.com>
2022-10-02 07:24:32 -0500

    card and coin vaue

--- CardgamesHH/CardgameDjangoApp/views.py
+++ CardgamesHH/CardgameDjangoApp/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render
 from CardGamesL.blackjack import Deck
+import random
 
 def index(request):
     return render(request,"index.html")
 
 def blackjack(request):
-    current_card = "7 of Hearts"
+    suits = ('hearts', 'diamond', 'spades', 'clover')
+    ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
+    values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10, 'Jack':10,
+            'Queen':10, 'King':10, 'Ace':11}
+    pic = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10, 'Jack':'J',
+            'Queen':'Q', 'King':'K', 'Ace':'A'}
+    if "current_card" in request.session:
+        current_card = request.session["current_card"]
+    else:
+        request.session["current_card"] = []
+        current_card = []
+    
+
     chips = request.session.get('chips', 0)
     if request.method == 'POST':
         if request.POST.get("1_chip"):
@@ -19,20 +32,29 @@ def blackjack(request):
         elif request.POST.get("100_chip"):  # You can use else in here too if there is only 2 submit types.
             chips += 100
         elif request.POST.get("hit"):
-            blackjack.hit(blackjack.deck, blackjack.playerHand)
-            player_cards = blackjack.getPlayerHand()
+            player_cards = []
+            suit = random.choice(suits)
+            rank = random.choice(ranks)
+            rank = pic[rank]
+            if rank in ("J","Q","K","A"):
+                rank = rank.lower()
+            player_cards.append(f"{rank}-{suit}")
             player_hands = []
             for i in player_cards:
                 player_hands.append(f"../static/CardgameDjangoApp/images/cards/{i}.png")
                 # Check to see if the player busts
+            current_card.append(player_hands[0])
+            request.session["current_card"] = current_card
+            chips = request.session["chips"]
             parameters = {
-                "current_card":current_card,
-                "chips":request.session['chips'],
-                "player_cards":player_hands
+                "current_card":chips,
+                "chips":chips,
+                "player_cards":current_card
             }
             return render(request, "blackjack.html",parameters)
         else:
             chips = 0
+            request.session["current_card"] = []
         request.session['chips'] = chips
     parameters = {
         "current_card":current_card,
